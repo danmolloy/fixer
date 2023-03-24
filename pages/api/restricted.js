5a51a0c6e5e9b1ea9c6703cee034ab4dce465bf5
Daniel Molloy <danielmolloy_6@icloud.com>
2023-03-24 13:16:39 +0000

    updated modules

--- pages/api/restricted.js
+++ pages/api/restricted.js
@@ -1,8 +1,8 @@
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./api/auth/[...nextauth]"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
 
 export default async (req, res) => {
-  const session = await unstable_getServerSession(req, res, authOptions)
+  const session = await getServerSession(req, res, authOptions)
   if (session) {
     res.send({
       content:
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,8 @@
-import EventsCalendar from "../../components/upcomingEvents/eventsCalendar"
 import EventsIndex from "../../components/upcomingEvents/eventsIndex"
 
 export default function Events() {
   return (
-    
     <EventsIndex />
   )
 }
 
-{/* <EventsCalendar /> */}
