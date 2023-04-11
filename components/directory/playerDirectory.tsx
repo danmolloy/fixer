f4f10c93d91f62ee9741d6a76d70c9e5fb11c492
Daniel Molloy <danielmolloy_6@icloud.com>
2023-04-11 13:20:21 +0100

    updated directory styling

--- components/directory/playerDirectory.tsx
+++ components/directory/playerDirectory.tsx
@@ -1,6 +1,7 @@
 import InstrumentDiv from "./instrumentDiv"
 import { instrumentArr } from "../fixing/fixing"
-import React from "react"
+import React, { useState } from "react"
+import PlayerTile from "./playerTile"
 
 interface PlayerDirectoryProps {
   data: {
@@ -12,6 +13,7 @@ interface PlayerDirectoryProps {
 
 export default function PlayerDirectory(props: PlayerDirectoryProps) {
   const { data } = props
+  const [selectedInstrument, setSelectedInstrument] = useState<string|null>(null)
 
   const directoryArr = instrumentArr.map(i => ({
     instrument: i,
@@ -21,9 +23,27 @@ export default function PlayerDirectory(props: PlayerDirectoryProps) {
 
   return (
     <div className="directory" id="player-directory" data-testid="player-directory-div">
-          {directoryArr.map(i => (
+          {/* {directoryArr.map(i => (
             <InstrumentDiv instrument={i.instrument} musicians={i.musicians} key={i.key}/>
+          ))} */}
+        <select onChange={e => setSelectedInstrument(e.target.value)} className="border shadow-sm p-1 rounded w-1/2 sm:w-1/3">
+          <option value={null}>Select instrument</option>
+          {instrumentArr.map(i => (
+            <option value={i} key={i}>
+              {i}
+            </option>
           ))}
+        </select>
+        <div className="flex flex-row flex-wrap justify-center">
+
+          {data.filter(j => j.instrument === selectedInstrument).length < 1
+          ? <h3 className="p-16 text-slate-700">No musicians found.</h3>
+          : data.filter(j => j.instrument === selectedInstrument).map(i => (
+            <div key={i.email}>
+              <PlayerTile player={i} />
+            </div>
+          ))}
+        </div>
       </div>
   )
 }
