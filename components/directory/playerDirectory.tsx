ea003b257853f9cfb23fbfcf6e9e2f73cbd94285
Daniel Molloy <danielmolloy_6@icloud.com>
2023-04-12 16:42:34 +0100

    directory bug fix

--- components/directory/playerDirectory.tsx
+++ components/directory/playerDirectory.tsx
@@ -5,6 +5,7 @@ import PlayerTile from "./playerTile"
 
 interface PlayerDirectoryProps {
   data: {
+    id: string
     name: string
     instrument: string
     email: string
@@ -15,17 +16,9 @@ export default function PlayerDirectory(props: PlayerDirectoryProps) {
   const { data } = props
   const [selectedInstrument, setSelectedInstrument] = useState<string|null>(null)
 
-  const directoryArr = instrumentArr.map(i => ({
-    instrument: i,
-    musicians: data.filter(j => j.instrument === i),
-    key: i
-  }))
 
   return (
     <div className="directory" id="player-directory" data-testid="player-directory-div">
-          {/* {directoryArr.map(i => (
-            <InstrumentDiv instrument={i.instrument} musicians={i.musicians} key={i.key}/>
-          ))} */}
         <select onChange={e => setSelectedInstrument(e.target.value)} className="border shadow-sm p-1 rounded w-1/2 sm:w-1/3">
           <option value={null}>Select instrument</option>
           {instrumentArr.map(i => (
@@ -35,11 +28,10 @@ export default function PlayerDirectory(props: PlayerDirectoryProps) {
           ))}
         </select>
         <div className="flex flex-row flex-wrap justify-center">
-
-          {data.filter(j => j.instrument === selectedInstrument).length < 1
+          {data.filter(i => i.instrument === selectedInstrument).length < 1
           ? <h3 className="p-16 text-slate-700">No musicians found.</h3>
           : data.filter(j => j.instrument === selectedInstrument).map(i => (
-            <div key={i.email}>
+            <div key={i.id}>
               <PlayerTile player={i} />
             </div>
           ))}
