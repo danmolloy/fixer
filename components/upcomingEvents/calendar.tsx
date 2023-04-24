b4824997d706c6e1c8742600422bac62d33f0bc4
Daniel Molloy <danielmolloy_6@icloud.com>
2023-04-24 12:09:02 +0100

    mobile calender datepicker added

--- components/upcomingEvents/calendar.tsx
+++ components/upcomingEvents/calendar.tsx
@@ -10,15 +10,33 @@ interface CalendarProps {
   setSelectedDate: (newDate: moment.Moment) => void
 }
 
+/* 
+Possibly insignificant bug: mobile selector gives new Date(), 
+whereas CalendarPicker gives Date() from when page was loaded
+*/
+
 export default function Calendar(props: CalendarProps) {
   const { selectedDate, setSelectedDate} = props
 
   return (
     <div className="flex flex-col w-full md:w-1/2 bg-white" data-testid="date-picker-div"> 
 {/*       <ButtonPrimary handleClick={() => {}} id="add-calendar-btn" text="Add to Diary" className="self-end w-28 text-blue-500 border-blue-500 hover:bg-blue-50" />
- */}      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <CalendarPicker date={selectedDate} onChange={(newDate) => setSelectedDate(moment(newDate))} />
-      </LocalizationProvider>
+ */}      
+      <div className="hidden md:flex">
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+          <CalendarPicker  date={selectedDate} onChange={(newDate) => setSelectedDate(moment(newDate))} /> 
+        </LocalizationProvider>
+      </div>
+      <div className="md:hidden p-2 flex flex-col items-center">
+        <input 
+          className="border border-zinc-400 rounded p-1 shadow-sm"
+          type="date" 
+          id="start" 
+          name="date-picker"
+          value={selectedDate.format("YYYY-MM-DD")}
+          onChange={(e) => setSelectedDate(moment(new Date(e.target.value)))}
+          min="2023-01-01" max="2028-12-31" />
+      </div>
     </div>
   )
 }
