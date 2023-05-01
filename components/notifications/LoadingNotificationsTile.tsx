26cd87790678d652212a27c4573ae71bd9041bdc
Daniel Molloy <danielmolloy_6@icloud.com>
2023-05-01 14:35:04 +0100

    isLoading notifications tile

---
+++ components/notifications/LoadingNotificationsTile.tsx
@@ -0,0 +1,24 @@
+import ButtonPrimary from "../index/buttonPrimary";
+import PulsingDiv from "../layout/pulsingDiv";
+
+export default function LoadingNotificationsTile() {
+  return (
+    <div className="border rounded shadow-sm m-1 ">
+      <div className="p-4">
+      <PulsingDiv classNames=" w-1/2 h-5"/>
+      <PulsingDiv classNames="w-2/3 mt-1 h-5" />
+      <PulsingDiv classNames="w-4/5 mt-1 h-5" />
+        
+        <div className="flex flex-row justify-evenly my-4">
+        <PulsingDiv classNames="w-24 h-9 self-center" />
+        <PulsingDiv classNames="w-24 h-9 self-center" />
+        </div>
+        <div className="flex flex-col items-center h-6">
+        <PulsingDiv classNames=" w-1/2 h-5"/>
+
+        </div>
+        </div>
+        
+      </div>
+  )
+}
--- components/notifications/notificationsIndex.tsx
+++ components/notifications/notificationsIndex.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import NotificationTile from "./notificationTile";
+import LoadingNotificationsTile from "./LoadingNotificationsTile";
 
 export default function NotificationsIndex(props) {
   const { data, mutate } = props;
   return (
     <div>
-      {data.playerCalls.sort((a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt))).map(i => (
+      {data 
+      ? data.playerCalls.sort((a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt))).map(i => (
         <div key={i.id}>
           <NotificationTile notification={i} mutate={() => mutate()} />
         </div>
-      ))}
+      ))
+      : <LoadingNotificationsTile />}
     </div>
   )
 }
