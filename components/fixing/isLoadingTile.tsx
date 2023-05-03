ebf503a2909dc4ec8b0ae414841d37c5804246a6
Daniel Molloy <danielmolloy_6@icloud.com>
2023-05-03 18:52:38 +0100

    css added

--- components/fixing/isLoadingTile.tsx
+++ components/fixing/isLoadingTile.tsx
@@ -2,7 +2,7 @@ import PulsingDiv from "../layout/pulsingDiv";
 
 export default function IsLoadingInstrumentTile() {
   return (
-    <div className="border flex flex-col w-full h-72 m-2 p-2 border-zinc-400 rounded-md">
+    <div className="border flex flex-col w-full md:w-1/2 h-72 m-2 p-2 border-zinc-400 rounded-md">
       <PulsingDiv classNames="h-8 w-24 "/>
       <PulsingDiv classNames="h-4 w-24 mt-2 "/>
       <PulsingDiv classNames="h-6 w-1/2 self-center mt-4"/>
