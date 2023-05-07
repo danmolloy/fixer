68daef0cc377a2b2a7b79c8e8804847ad424e7b0
Daniel Molloy <danielmolloy_6@icloud.com>
2023-05-07 12:44:11 +0100

    page title css

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -29,8 +29,8 @@ export default function Layout(props: LayoutProps) {
     <div className="min-h-screen w-screen flex flex-col justify-between font-nunito " data-testid="layout-div">
       <Header setShowMenu={(bool) => setShowMenu(bool)} showMenu={showMenu} session={session ? true : false} notifications={session && session.userData.playerCalls.filter(i => i.accepted === null).length > 0 ? true: false}/>
       {showMenu && <Menu signedIn={session ? true : false} setShowMenu={() => setShowMenu(false)} menuItems={session ? menuItems : landingMenuItems}/>}
-      <div className={showMenu ? "w-full p-3 blur":"w-full p-3"}>
-        <h1 className="ml-2 text-2xl">{pageTitle}</h1>
+      <div className={showMenu ? "w-full p-3 blur text-center":"w-full p-3 text-center"}>
+        <h1 className="ml-2 tex-center text-3xl ">{pageTitle}</h1>
       </div>
       <div onFocus={() => setShowMenu(false)} className={showMenu ? "layout-children w-screen p-2 flex flex-col items-center bg-white blur": "layout-children w-screen p-2 flex flex-col items-center bg-white pb-12"} data-testid="main-div">
         {children}
