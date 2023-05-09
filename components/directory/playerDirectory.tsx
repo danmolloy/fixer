14649aed64eb6e54cf08fc2200245e809532d1c6
Daniel Molloy <danielmolloy_6@icloud.com>
2023-05-09 21:54:44 +0100

    toggle btn refinement

--- components/directory/playerDirectory.tsx
+++ components/directory/playerDirectory.tsx
@@ -40,7 +40,7 @@ export default function PlayerDirectory(props: PlayerDirectoryProps) {
             </option>
           ))}
         </select>
-      <ToggleSwitch label="Alphabetical" toggled={sortedList} setToggled={() => setSortedList(!sortedList)} />
+      {selectedInstrument !== null && <ToggleSwitch label="Alphabetical" toggled={sortedList} setToggled={() => setSortedList(!sortedList)} />}
       </div>
         {selectedInstrument === ""
         ? <div>
