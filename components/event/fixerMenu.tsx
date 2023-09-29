a2faf9dbbd93459beadecbce1a49037a6d67b9ab
Daniel Molloy <danielmolloy_6@icloud.com>
2023-09-29 17:36:21 +0100

    added cancel event functionality

--- components/event/fixerMenu.tsx
+++ components/event/fixerMenu.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router"
 import React, { useEffect, useRef } from "react"
 import { AiOutlineClose } from "react-icons/ai"
 
+
 export type FixerMenuProps = {
   eventId: number
 }
@@ -12,6 +13,7 @@ export type FixerMenuProps = {
 
 export default function FixerMenu(props: FixerMenuProps) {
   const { eventId } = props;
+  const router = useRouter()
 
   const messageAll = () => {
     const fixerMessage = prompt(`What is your message to all booked musicians?`)
@@ -28,20 +30,53 @@ export default function FixerMenu(props: FixerMenuProps) {
     }
   }
 
+  const cancelEvent = () => {
+    const fixerMessage = prompt(`What is your message to all booked musicians about the cancellation?`)
+    const reqBody = {
+      message: 
+      `Dan Molloy has cancelled event ${eventId}. They send the following message to all players: 
+      "${fixerMessage}"`,
+      eventId: eventId
+    }
+    if (fixerMessage === null || fixerMessage.length === 0) {
+      return;
+    } else {
+      return axios.post("/api/event/cancel", reqBody).then(() => router.push("/"));
+    }
+  }
+
+  const exportToCSV = () => {}
+    
+
+  const exportToCalendar = () => {}
+
   return (
     <div className="" data-testid="menu-div">
       <Link href={`/event/edit/${eventId}`}>
-          <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" data-testid={"edit-event-link"}>
-            Edit Event
-          </div>
-        </Link> 
-        <button onClick={() => messageAll()} className="cursor-pointer w-full text-start" data-testid={"msg-all-btn"}>
-            <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" >
-              Message All
-            </div>
-          </button>
-
-      
+        <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" data-testid={"edit-event-link"}>
+          Edit Event
+        </div>
+      </Link> 
+      <button onClick={() => messageAll()} className="cursor-pointer w-full text-start" data-testid={"msg-all-btn"}>
+        <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" >
+          Message All
+        </div>
+      </button>
+      <button onClick={() => exportToCSV()} className="cursor-pointer w-full text-start" data-testid={"msg-all-btn"}>
+        <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" >
+          Export to CSV
+        </div>
+      </button>
+      <button onClick={() => exportToCalendar()} className="cursor-pointer w-full text-start" data-testid={"msg-all-btn"}>
+        <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" >
+          Export to Calendar
+        </div>
+      </button>
+      <button onClick={() => cancelEvent()} className="cursor-pointer w-full text-start" data-testid={"msg-all-btn"}>
+        <div className="w-full hover:bg-slate-100 py-4 pl-4 font-light" >
+          Cancel Event
+        </div>
+      </button>
     </div>
   )
 }
