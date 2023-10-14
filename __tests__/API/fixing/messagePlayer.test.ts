4416c764b4a309b9b768b2597f5fe28b851b33c2
Daniel Molloy <danielmolloy_6@icloud.com>
2023-10-14 20:32:00 +0100

    updated messagePlayer to use universal func

--- __tests__/API/fixing/messagePlayer.test.ts
+++ __tests__/API/fixing/messagePlayer.test.ts
@@ -1,12 +1,9 @@
-import { messagePlayer } from '../../../pages/api/fixing/messagePlayer'
 import { twilioClient } from "../../../twilio";
 
 const mockMessage = "Hey baby"
 
 jest.mock("../../../twilio", () => require("../../../__mocks__/twilio"))
 
-describe("messagePlayer", () => {
-  it("Resolves", async () => {
-    expect(await messagePlayer(mockMessage)).resolves
-  })
+describe("handler", () => {
+  it("calls sendMessage with args", async () => {})
 })
--- pages/api/fixing/messagePlayer.tsx
+++ pages/api/fixing/messagePlayer.tsx
@@ -1,27 +1,5 @@
-import { twilioClient } from "../../../twilio"
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-export const mockFunction = () => {
-  return 1;
-}
-
-export const messagePlayer = (message) => {
-  if (process.env.TWILIO_ACTIVE === "false" && process.env.NODE_ENV !== "test") {
-    console.log("Twilio not active")
-    return;
-  }
-
-  const MSS = 'MGa3507a546e0e4a6374af6d5fe19e9e16';
-  const toNum = process.env.PHONE
-  
-  return twilioClient.messages 
-  .create({ 
-     body: message, 
-     messagingServiceSid: MSS,      
-     to: toNum 
-   }) 
-}
+import { sendMessage } from "./messages";
 
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
@@ -29,7 +7,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     message
   } = req.body
 
-  res.status(200).json(await messagePlayer(message))
+  res.status(200).json(await sendMessage(message, process.env.PHONE))
   
 
 }
