45d424465271cdf6a9562abfc4b7ba31e98ad7ac
Daniel Molloy <danielmolloy_6@icloud.com>
2023-10-16 17:14:43 +0100

    created fixerPlayerMenu & tests

---
+++ __mocks__/axios.ts
@@ -0,0 +1,5 @@
+export default {
+  get: jest.fn(),
+  post: jest.fn(),
+  // Add other axios methods you need to mock
+};
---
+++ components/fixing/finxingPlayerMenu.tsx
@@ -0,0 +1,83 @@
+import { PlayerCall } from "@prisma/client"
+import axios from "axios";
+import Link from "next/link";
+
+export type FixingPlayerMenuProps = {
+  playerCall: PlayerCall
+  setShowMenu: (arg: boolean) => void
+
+}
+
+export default function FixingPlayerMenu(props: FixingPlayerMenuProps) {
+  const { playerCall, setShowMenu } = props;
+
+  const updatePlayer = async (data: {}) => {
+    const reqBody = {playerCallId: playerCall.id, data: data}
+    await axios.post("/api/fixing/updatePlayerCall", reqBody)
+    setShowMenu(false)
+  }
+
+  const sendMessage = async (message?: string) => {
+    if (!message) {
+      message = `Dan Molloy sends the following message: "${prompt(`What is your message to this player?`)}"`
+    }
+    const reqBody = {
+      message: message
+    }
+    if (reqBody.message === null || reqBody.message.length === 0) {
+      return;
+    } else {
+      return axios.post("/api/fixing/messagePlayer", reqBody);
+    }
+  }
+
+  const deletePlayerCall = async() => {
+    const reqBody = {playerCallId: playerCall.id}
+
+    await axios.post("/api/fixing/removePlayer", reqBody)
+  }
+
+  return (
+    <div data-testid="fixing-player-menu">
+      <Link data-testid="profile-link"  href={`/user/${playerCall.musicianId}`}>
+        View profile
+      </Link>
+      <button onClick={() => {updatePlayer({accepted: true})}}>
+        {playerCall.bookingOrAvailability === "Booking" 
+        ? "Confirm booking":"Confirm availability"}
+      </button>
+      {playerCall.bookingOrAvailability === "Availability"  
+      && <button onClick={() => {updatePlayer({recieved: false, accepted: false, bookingOrAvailability: "Booking"})}}>
+        Make offer to book
+      </button>}
+      <button onClick={() => deletePlayerCall()}>
+      {playerCall.bookingOrAvailability === "Booking" 
+      && playerCall.recieved === true && playerCall.accepted !== false
+        ? "Cancel offer":"Remove from list"}
+      </button>
+      {playerCall.bookingOrAvailability === "Booking" 
+      && playerCall.accepted === true 
+      && <button onClick={() => updatePlayer({status: "DEP OUT"})}>
+        Dep player
+      </button>}
+      <button onClick={() => sendMessage()}>
+        Send message
+      </button>
+      <button onClick={() => sendMessage(`Dan Molloy is reminding you to respond to his ${playerCall.bookingOrAvailability === "Booking" ? "offer": "availability check"}.`)}>
+        Poke
+      </button>
+    </div>
+  )
+}
+
+/* 
+View Profile
+Unbook/Remove from list
+Book/Confirm availability player
+Offer to book
+Dep Out/Replace 
+Send Message
+Poke player
+
+
+*/
