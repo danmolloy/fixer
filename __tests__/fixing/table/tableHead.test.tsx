82c41d90ef23a25bc45c8ba8ba1fb7f400eb9e79
Daniel Molloy <danielmolloy_6@icloud.com>
2023-10-18 10:18:15 +0100

    table head created and tested

---
+++ __tests__/fixing/table/tableHead.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { mockCall } from "../../../__mocks__/models/call";
+import TableHead, { TableHeadProps } from "../../../components/fixing/table/tableHead";
+import { DateTime } from "luxon";
+import "@testing-library/jest-dom"
+
+const mockProps: TableHeadProps = {
+  eventCalls: [mockCall]
+}
+
+describe("<TableHead />", () => {
+  beforeEach(() => {
+    render(<TableHead {...mockProps} />)
+  })
+  it("table-head is in document", () => {
+    const tableHead = screen.getByTestId("table-head")
+    expect(tableHead).toBeInTheDocument()
+  })
+  it("name cell is in document", () => {
+    const nameCell = screen.getByTestId("name-cell")
+    expect(nameCell).toBeInTheDocument()
+  })
+  it("each event call has a cell and start time is format 'DAY DD/MM/YYYY HH:MM PM'", () => {
+    for (let i = 0; i < mockProps.eventCalls.length; i++) {
+      let formattedStartTime = DateTime.fromJSDate(new Date(mockProps.eventCalls[i].startTime)).toFormat("ccc f")
+      console.log(formattedStartTime)
+      let callCell = screen.getByText(formattedStartTime)
+      expect(callCell).toBeInTheDocument()
+    }
+  })
+  it("action cell is in document", () => {
+    const actionCell = screen.getByTestId("action-cell")
+    expect(actionCell).toBeInTheDocument()
+  })
+})
