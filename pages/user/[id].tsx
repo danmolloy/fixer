6305acacdaaac66cd090a7d3f6e47de27bbeac15
Daniel Molloy <danielmolloy_6@icloud.com>
2023-11-19 18:14:29 +0000

    typeError

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -2,9 +2,6 @@ import UserProfile from "../../components/users/profile";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import IsLoadingProfile from "../../components/users/isLoadingProfile";
-import Loading from "../../components/index/loading";
-import { useSession } from "next-auth/react";
-import LandingPage from "../../components/externalSite/landingPage";
 import LayoutIndex from "../../components/layout";
 
 const fetcher = (url: string):Promise<any> => fetch(url).then((res) => res.json())
