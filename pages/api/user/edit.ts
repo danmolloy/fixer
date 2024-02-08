6bee56ae74fd0de20a47f09f958d89288d4117ff
Daniel Molloy <danielmolloy_6@icloud.com>
2024-02-08 18:24:20 +0000

    User name removed from update User details API

--- pages/api/user/edit.ts
+++ pages/api/user/edit.ts
@@ -2,19 +2,20 @@ import prisma from "../../../client"
 
 type updateUserObj = {
   userId: string
-  name: string
+  firstName: string
+  lastName: string
   email: string
   instrument: string
 }
 
 const updateUser = async(userObj) => {
-  
   return await prisma.user.update({
     where: {
       id: userObj.userId
     },
     data: {
-      name: userObj.name,
+      firstName: userObj.firstName,
+      lastName: userObj.lastName,
       email: userObj.email,
       instrumentsList: [userObj.instrument]
     },
@@ -25,14 +26,16 @@ const updateUser = async(userObj) => {
 export default async function handle(req, res) {
   const { 
     userId,
-    name,
+    firstName,
+    lastName,
     email,
     instrument
   } = req.body
 
   const userObj: updateUserObj = {
     userId,
-    name,
+    firstName,
+    lastName,
     email,
     instrument
   }
